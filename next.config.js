f6f138be9b852939b8efcb3871bd2cbe465e6878
aman <amanvishen46@gmail.com>
2024-06-05 17:04:08 +0530

    added new files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
 //   experimental: {
 //     serverActions: true,
 // },
+module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"]
+    }
+  ],
+},
   images: {
     formats:['image/webp'],
     domains: ["localhost"],
